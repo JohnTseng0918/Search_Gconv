1bc50025ca96e0b3fbbeb4fc25d2cec8bc040cbb
JohnTseng0918 <s9141302@gmail.com>
2021-03-30 16:59:45 +0800

    add get_group_choice and factors

--- utils.py
+++ utils.py
@@ -1,4 +1,12 @@
 import torch
+from functools import reduce
+
+def factors(n):
+    """
+    Copied from https://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python
+    """    
+    return set(reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 
 def accuracy(output, target, topk=(1,)):
     maxk = max(topk)
