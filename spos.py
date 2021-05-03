faf6c0fd50d04f68e927fe7d66bbc0794fc4f8ce
JohnTseng0918 <s9141302@gmail.com>
2021-05-03 18:23:43 +0800

    add spos.py

---
+++ spos.py
@@ -0,0 +1,163 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import utils
+import random
+from models.resnet import *
+
+class SuperNet:
+    def __init__(self, args):
+        self.dataset = args.dataset
+        self.train_batch_size = args.train_batch_size
+        self.validate_batch_size = args.validate_batch_size
+        self.epoch = args.epoch
+        self.genome_type=[]
+        self.genome_idx_type=[]
+
+    def load_model(self):
+        self.model = cifar_resnet56(pretrained=True)
+
+    def build_oneshot(self):
+        self.group_mod_list = nn.ModuleList()
+        for name, mod in self.model.named_modules():
+            if isinstance(mod, torch.nn.modules.conv.Conv2d):
+                n, c, h, w = mod.weight.shape
+                if h==1 or w ==1:
+                    continue
+                g_list = utils.get_groups_choice_list(mod.in_channels, mod.out_channels)
+                sub_mod_list = nn.ModuleList()
+                for g in g_list:
+                    new_mod = nn.Conv2d(mod.in_channels, mod.out_channels, mod.kernel_size, padding = mod.padding, bias = mod.bias, groups = g)
+                    nn.init.xavier_normal_(new_mod.weight)
+                    if g == 1:
+                        new_mod.weight = torch.nn.Parameter(mod.weight)
+                        new_mod.requires_grad_(False)
+                    sub_mod_list.append(new_mod)
+                self.group_mod_list.append(sub_mod_list)
+
+    def warnup_oneshot(self):
+        idx=0
+        for name, mod in self.model.named_modules():
+            if isinstance(mod, torch.nn.modules.conv.Conv2d):
+                n, c, h, w = mod.weight.shape
+                if h==1 or w ==1:
+                    continue
+                if (len(self.group_mod_list[idx])==1):
+                    idx+=1
+                    continue
+                #print(idx, len(self.group_mod_list[idx]))
+                for choice in self.group_mod_list[idx]:
+                    #print(choice)
+                    if choice.groups==1:
+                        continue
+                    mod.weight = torch.nn.Parameter(choice.weight)
+                    mod.groups = choice.groups
+                    print(choice)
+                    print(choice.weight.shape)
+                    self.validate()
+                    self.train()
+                    self.validate()
+                    choice.weight = torch.nn.Parameter(mod.weight)
+
+                mod.weight = torch.nn.Parameter(self.group_mod_list[idx][0].weight)
+                mod.groups = 1
+
+                idx+=1        
+
+    def self_define_model(self):
+        idx = 0
+        self.genome_type=[]
+        self.genome_idx_type=[]
+        for name, mod in self.model.named_modules():
+            if isinstance(mod, torch.nn.modules.conv.Conv2d):
+                n, c, h, w = mod.weight.shape
+                if h==1 or w ==1:
+                    continue
+                t = 0
+                if idx == 5:
+                    t = 2
+                mod.weight = torch.nn.Parameter(self.group_mod_list[idx][t].weight)
+                mod.groups = self.group_mod_list[idx][t].groups
+                self.genome_type.append(mod.groups)
+                self.genome_idx_type.append(t)
+                idx+=1
+
+    def random_model(self):
+        idx = 0
+        self.genome_type=[]
+        self.genome_idx_type=[]
+        for name, mod in self.model.named_modules():
+            if isinstance(mod, torch.nn.modules.conv.Conv2d):
+                n, c, h, w = mod.weight.shape
+                if h==1 or w ==1:
+                    continue
+                t = random.randint(0, len(self.group_mod_list[idx])-1)
+                mod.weight = torch.nn.Parameter(self.group_mod_list[idx][t].weight)
+                mod.groups = self.group_mod_list[idx][t].groups
+                self.genome_type.append(mod.groups)
+                self.genome_idx_type.append(t)
+                idx+=1
+
+    def print_choice(self):
+        print(self.group_mod_list)
+
+    def print_model(self):
+        print(self.model)
+    
+    def print_num_choice(self):
+        num_layer=0
+        layer_sum=0
+        total=0
+        for sublist in self.group_mod_list:
+            layer_sum=0
+            for item in sublist:
+                layer_sum+=1
+            total+=layer_sum
+            num_layer+=1
+        print("number of layer:", num_layer, "total choice:", total)
+
+    def train(self):
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(self.model.parameters())
+        utils.train(self.trainloader, self.model, optimizer, criterion, self.epoch)
+
+    def validate(self):
+        criterion = nn.CrossEntropyLoss()
+        acc1, acc5, loss = utils.validate(self.testloader, self.model, criterion)
+        print("validate:")
+        print("top1 acc:", acc1)
+        print("top5 acc:", acc5)    
+        print("avg loss:", loss)
+        print("-------------------------------------------------")
+
+    def get_dataloader(self):
+        if self.dataset == "cifar10":
+            train_transforms = torchvision.transforms.Compose([
+                torchvision.transforms.RandomHorizontalFlip(),
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
+            ])
+            test_transforms = torchvision.transforms.Compose([
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
+            ])
+            self.train_set = torchvision.datasets.CIFAR10(root='./data/cifar10',train=True,transform=train_transforms,download=True)
+            self.test_set = torchvision.datasets.CIFAR10(root='./data/cifar10',train=False,transform=test_transforms,download=True)
+        elif self.dataset == "cifar100":
+            train_transforms = torchvision.transforms.Compose([
+                torchvision.transforms.RandomHorizontalFlip(),
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762))
+            ])
+            test_transforms = torchvision.transforms.Compose([
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762))
+            ])
+            self.train_set = torchvision.datasets.CIFAR100(root='./data/cifar100',train=True,transform=train_transforms,download=True)
+            self.test_set = torchvision.datasets.CIFAR100(root='./data/cifar100',train=False,transform=test_transforms,download=True)
+        elif self.dataset == "imagenet":
+            pass
+
+        self.trainloader = torch.utils.data.DataLoader(self.train_set, batch_size=self.train_batch_size,shuffle=True)
+        self.testloader = torch.utils.data.DataLoader(self.test_set, batch_size=self.validate_batch_size,shuffle=False)
