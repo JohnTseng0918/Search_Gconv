7e7656d19ce08f73f4082e5b523a0bbc2b48df8e
JohnTseng0918 <s9141302@gmail.com>
2021-08-12 17:43:44 +0800

    resnet50 supernet fix

--- models/resnet_oneshot.py
+++ models/resnet_oneshot.py
@@ -101,6 +101,27 @@ class Bottleneck(nn.Module):
             self.choice.append(conv)
         return len(self.choice)
 
+    def grow_choice_with_pretrained(self):
+        outchannel, inchannel, _, _ = self.choice[0].weight.shape
+        g_list = utils.get_groups_choice_list(inchannel, outchannel)
+        if len(g_list) > len(self.choice):
+            g = g_list[len(self.choice)]
+            conv = conv3x3(inchannel, outchannel, self.stride, g, 1)
+            # move groups = 1 weight to groups = n
+            W = self.choice[0].weight
+            outc_start, intc_start = 0, 0
+            outc_interval = int(outchannel/g)
+            intc_interval = int(inchannel/g)
+            tensorlist=[]
+            for i in range(g):
+                tensorlist.append(W[outc_start:outc_start+outc_interval, intc_start:intc_start+intc_interval,:,:])
+                outc_start+=outc_interval
+                intc_start+=intc_interval
+            wnew = torch.cat(tuple(tensorlist),0)
+            conv.weight = torch.nn.Parameter(wnew)
+            self.choice.append(conv)
+        return len(self.choice)
+
     def forward(self, x: Tensor, arch) -> Tensor:
         identity = x
 
@@ -198,84 +219,17 @@ class resnet50_oneshot(nn.Module):
         for i in range(len(self.features)):
             n = self.features[i].grow_choice()
             self.all_choice_list.append(n)
-    
-    def get_all_arch(self):
-        return tuple(self.all_choice_list)
 
-
-
-class resnet164_oneshot(nn.Module):
-    def __init__(self, num_classes = 100):
-        super(resnet164_oneshot, self).__init__()
-        norm_layer = nn.BatchNorm2d
-        self._norm_layer = norm_layer
-        self.dilation = 1
-        self.inplanes = 16
-        self.groups = 1
-        self.base_width = 16
-        self.num_classes = num_classes
-
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
-
-        self.features = torch.nn.ModuleList()        
-        layer1 = self._make_layer(Bottleneck, 16, 18)
-        layer2 = self._make_layer(Bottleneck, 32, 18, stride=2)
-        layer3 = self._make_layer(Bottleneck, 64, 18, stride=2)
-        for i in range(len(layer1)):
-            self.features.append(layer1[i])
-        for i in range(len(layer2)):
-            self.features.append(layer2[i])
-        for i in range(len(layer3)):
-            self.features.append(layer3[i])
-        self.avgpool = nn.AvgPool2d(kernel_size=8, stride=1, padding=0)
-        self.fc = nn.Linear(256, num_classes)
-
-    def _make_layer(self, block: Type[Union[BasicBlock, Bottleneck]], planes: int, blocks: int,
-                    stride: int = 1, dilate: bool = False) -> nn.Sequential:
-        norm_layer = self._norm_layer
-        downsample = None
-        previous_dilation = self.dilation
-        if dilate:
-            self.dilation *= stride
-            stride = 1
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
-                            self.base_width, previous_dilation, norm_layer))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, groups=self.groups,
-                                base_width=self.base_width, dilation=self.dilation,
-                                norm_layer=norm_layer))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x, architecture):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-
-        for i, arch_id in enumerate(architecture):
-            x = self.features[i](x, arch_id)
-
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.fc(x)
-
-        return x
-
-    def grow(self):
+    def grow_with_pretrained(self):
         self.all_choice_list = []
         for i in range(len(self.features)):
-            n = self.features[i].grow_choice()
+            n = self.features[i].grow_choice_with_pretrained()
             self.all_choice_list.append(n)
     
     def get_all_arch(self):
         return tuple(self.all_choice_list)
+
+    def get_origin_arch(self):
+        num_feature = len(self.features)
+        res = tuple([0] * num_feature)
+        return res
